6f8287aeed71bb517200733c5bab3e9e3e791580
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-11-30 20:06:38 +0600

    google login feature added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router';
 import useAuth from '../../hooks/useAuth';
 
 export default function Register() {
-  const { createUser } = useAuth();
+  const { createUser, signInWithGoogle } = useAuth();
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -39,6 +39,15 @@ export default function Register() {
     console.log('Form Data Submitted:', formData);
     // Here you can add logic to handle registration, e.g., call an API
   };
+  const handleGoogleLogin = async () => {
+    try {
+      const res = await signInWithGoogle();
+      console.log(res);
+      navigate('/');
+    } catch (error) {
+      console.error('Error logging in with Google:', error);
+    }
+  };
   return (
     <>
       <div className="hero bg-base-200 min-h-screen">
@@ -46,11 +55,8 @@ export default function Register() {
           <div className="text-center ">
             <h1 className="text-5xl font-bold">Register now!</h1>
           </div>
-          <form
-            onSubmit={handleSubmit}
-            className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl"
-          >
-            <div className="card-body">
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form onSubmit={handleSubmit} className="card-body">
               <fieldset className="fieldset">
                 <label className="label">Name</label>
                 <input
@@ -90,8 +96,41 @@ export default function Register() {
                 />
                 <button className="btn btn-neutral mt-4">Register</button>
               </fieldset>
-            </div>
-          </form>
+            </form>
+            <button
+              onClick={handleGoogleLogin}
+              className="btn mx-6 mb-5 bg-white text-black border-[#e5e5e5]"
+            >
+              <svg
+                aria-label="Google logo"
+                width="16"
+                height="16"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <g>
+                  <path d="m0 0H512V512H0" fill="#fff"></path>
+                  <path
+                    fill="#34a853"
+                    d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                  ></path>
+                  <path
+                    fill="#4285f4"
+                    d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                  ></path>
+                  <path
+                    fill="#fbbc02"
+                    d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                  ></path>
+                  <path
+                    fill="#ea4335"
+                    d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                  ></path>
+                </g>
+              </svg>
+              Login with Google
+            </button>
+          </div>
         </div>
       </div>
     </>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,7 +1,9 @@
 import {
   createUserWithEmailAndPassword,
+  GoogleAuthProvider,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
 } from 'firebase/auth';
 import { useEffect, useState } from 'react';
@@ -10,6 +12,12 @@ import { AuthContext } from '../context';
 
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const signInWithGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
 
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
@@ -30,7 +38,14 @@ export default function AuthProvider({ children }) {
     return () => unsubscribe();
   }, []);
 
-  const authInfo = { user, setUser, createUser, loginUser, logoutUser };
+  const authInfo = {
+    user,
+    setUser,
+    createUser,
+    loginUser,
+    logoutUser,
+    signInWithGoogle,
+  };
   return (
     <>
       <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
