213d0cf1e35d6b51d5e5ab1529734c450a82b53c
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-11-30 15:18:00 +0600

    navbar design and register form design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { Outlet } from 'react-router';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
-      <Outlet />
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router';
 import App from '../App';
+import Register from '../components/Register/Register';
 import Home from '../page/Home';
 
 export const router = createBrowserRouter([
@@ -13,4 +14,5 @@ export const router = createBrowserRouter([
       },
     ],
   },
+  { path: 'register', Component: Register },
 ]);
